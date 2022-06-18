813002a1ac9a5590cbc45cbb2859bc9ab8b7835a
badplayer-august <augusthsueh.cs08@nctu.edu.tw>
2022-06-18 19:52:33 +0800

    fix error when killing the process

--- ascii_video.py
+++ ascii_video.py
@@ -1,119 +1,170 @@
-import argparse
-import threading
-import math
-import time
+import array
 import cv2
+import argparse
+import math
 from curses import wrapper
-
-ascii_grayscale = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
-# ascii_grayscale = ['$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', '\'', '.', ' ']
+from threading import Event, Thread, Lock
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n','--name', default='video.mp4')
 # parser.add_argument('-i', '--info', default=0, type=bool)
 args = parser.parse_args()
 
+ascii_grayscale = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']
 binning = 256/len(ascii_grayscale)
-to_ascii = []
+TO_ASCII = []
+
 
 for i in range(256):
-    to_ascii.append(ascii_grayscale[int(i//binning)])
+    TO_ASCII.append(ascii_grayscale[int(i//binning)])
 
-vid = cv2.VideoCapture(args.name)
-if vid.isOpened():
-    frame_width = vid.get(3)
-    frame_height = vid.get(4)
-    fps = vid.get(5)
+# TO_ASCII = array.array('b', TO_ASCII)
+buffer = []
+is_end = False
+is_finish = False
+access_buffer = Lock()
+not_full = Event()
+not_full.set()
+not_update = Event()
 
-    gcd = math.gcd(int(frame_height), int(frame_width))
+class BufferFrame(Thread):
+    BUFFER_SIZE = 64
 
-    frame_height_rate = int(frame_height/gcd)
-    frame_width_rate = int(frame_width/gcd)*2
-else:
-    exit()
+    def __init__(self, video, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.video = video
 
-buffer_size = 1<<6
-n_finish = True
-n_refresh = False
-buff = [[] for _ in range(buffer_size)]
-r, w = 0, 0
+    def run(self):
+        global buffer
+        global is_end
+        global access_buffer
+        global not_full
 
-def buffer():
-    global w
-    global buff
+        for _ in range(int(self.video.get(7))):
+            has_frames, frame = self.video.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    for _ in range(int(vid.get(7))):
-        while (w + 1 & 63) == r:
-            time.sleep(0.001)
+            not_full.wait()
+            with access_buffer:
+                buffer.append(frame)
 
-        has_frames, frame = vid.read()
-        frame = cv2.cvtColor(
-            frame,
-            cv2.COLOR_BGR2GRAY,
-        )
-        buff[w] = frame
-        w = (w + 1 & 63)
- #      print('w={}'.format(w))
+            if len(buffer) == self.BUFFER_SIZE:
+                not_full.clear()
 
-def load(stdscr):
-    global r
-    global n_refresh
-    global n_finish
+        is_end = True
 
-    for _ in range(int(vid.get(7))):
-        while r == w or n_refresh:
-            time.sleep(0.001)
+class UpdateScreen(Thread):
+    def __init__(self, stdscr, frame_width_rate, frame_height_rate, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stdscr = stdscr
+        self.frame_width_rate = frame_width_rate
+        self.frame_height_rate = frame_height_rate
 
-        stdscr.erase()
-        scr_height, scr_width = stdscr.getmaxyx()
+    def run(self):
+        global buffer
+        global is_end
+        global access_buffer
+        global not_full
+        global not_update
+        global is_finish
 
-        if scr_height*frame_width_rate < scr_width*frame_height_rate:
-            resize_rate = scr_height//frame_height_rate
-        else:
-            resize_rate = scr_width//frame_width_rate
+        while not (is_end and len(buffer) == 0):
+            self.stdscr.erase()
+            scr_height, scr_width = self.stdscr.getmaxyx()
+            if scr_height*self.frame_width_rate < scr_width*self.frame_height_rate:
+                resize_rate = scr_height//self.frame_height_rate
+            else:
+                resize_rate = scr_width//self.frame_width_rate
 
-        resize_frame_height = int(resize_rate*frame_height_rate)
-        resize_frame_width = int(resize_rate*frame_width_rate)
-        padding_y = int(scr_height - resize_frame_height)>>1
-        padding_x = int(scr_width - resize_frame_width)>>1
+            resize_frame_height = int(resize_rate*self.frame_height_rate)
+            resize_frame_width = int(resize_rate*self.frame_width_rate)
+            padding_y = int(scr_height - resize_frame_height)>>1
+            padding_x = int(scr_width - resize_frame_width)>>1
 
-        buff_frame = cv2.resize(
-            buff[r],
-            dsize=(resize_frame_width, resize_frame_height),
-            interpolation=cv2.INTER_CUBIC
-        )
+            while len(buffer) == 0: continue
+            with access_buffer:
+                frame = buffer.pop(0)
+                not_full.set()
 
-        for y, row in enumerate(buff_frame):
-            for x, ele in enumerate(row):
-                stdscr.addch(
-                    y + padding_y,
-                    x + padding_x,
-                    to_ascii[ele],
-                )
-        stdscr.addstr(0, 0, '{}'.format((w+64-r)&63))
-        stdscr.refresh()
-        n_refresh = True
-        r = (r + 1 & 63)
- #      print('r={}'.format(r))
+            frame = cv2.resize(
+                frame,
+                dsize=(resize_frame_width, resize_frame_height),
+                interpolation=cv2.INTER_CUBIC
+            )
 
-    n_finish = False
+            not_update.wait()
+            not_update.clear()
 
-def refresh(stdscr):
-    global n_refresh
-    global n_finish
+            for y, row in enumerate(frame):
+                for x, ele in enumerate(row):
+                    self.stdscr.addch(
+                        y + padding_y,
+                        x + padding_x,
+                        TO_ASCII[ele],
+                    )
+            self.stdscr.addstr(0, 0, '{}'.format(len(buffer)))
+            self.stdscr.refresh()
 
-    while n_finish:
-        time.sleep(1/fps)
-        n_refresh = False
+        is_finish = True
+
+class Clock(Thread):
+    def __init__(self, spf, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.spf = spf
+
+    def run(self):
+        global not_update
+        global is_finish
+
+        clock = Event()
+
+        while not clock.wait(self.spf):
+            if is_finish: break
+            not_update.set()
+
+class Fake_Screen():
+    def __init__(self):
+        self.width = 20
+        self.height = 15
+        self.text = [['' for _ in range(20)] for _ in range(15)]
+
+    def erase(self):
+        self.text = [['' for _ in range(20)] for _ in range(15)]
+
+    def addch(self, x, y, c):
+        self.text[x][y] = c
+
+    def getmaxyx(self):
+        return 15, 20
+
+    def refresh(self):
+        for i in range(15):
+            print(self.text[i])
 
 def main(stdscr):
-    buffer_thread = threading.Thread(target=buffer)
-    load_thread = threading.Thread(target=load, args=(stdscr, ))
-    refresh_thread = threading.Thread(target=refresh, args=(stdscr, ))
+    stdscr.clear()
+    video = cv2.VideoCapture(args.name)
+    if video.isOpened():
+        frame_width = video.get(3)
+        frame_height = video.get(4)
+        fps = video.get(5)
 
+        gcd = math.gcd(int(frame_height), int(frame_width))
+
+        frame_height_rate = int(frame_height/gcd)
+        frame_width_rate = int(frame_width/gcd)*2
+    else:
+        exit()
+
+    buffer_thread = BufferFrame(video)
+    load_thread = UpdateScreen(stdscr, frame_width_rate, frame_height_rate)
+    refresh_thread = Clock(1/fps)
     buffer_thread.start()
     load_thread.start()
     refresh_thread.start()
-    stdscr.close()
+
+    buffer_thread.join()
+    load_thread.join()
+    refresh_thread.join()
 
 wrapper(main)
